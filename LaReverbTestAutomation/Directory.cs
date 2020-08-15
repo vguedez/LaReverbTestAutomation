8479eb892390d4a133973667e5606d29768d8aba
vguedez <vicente.guedez@gmail.com>
2020-08-15 18:38:36 +0200

    Directory.cs is added to tests

---
+++ LaReverbTestAutomation/Directory.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using TestFramework;
+using TestFramework.Pages;
+using TestFramework.Generators;
+
+namespace TestAutomation
+{
+    [TestClass()]
+    public class Directory
+    {
+        [ClassInitialize()]
+        public static void Initialize(TestContext context)
+        {
+            Browser.Initialize();
+            Pages.Login.GoTo();
+            Pages.Login.LogIn();
+            Pages.Dashboard.GoTo();
+        }
+
+        [TestMethod]
+        public void TestLeftSideMenu()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestDirectoryCount()
+        {
+
+        }
+
+        [TestMethod]
+        public void CanEditPublishedAd()
+        {
+
+        }
+
+        private void FillAndSaveAd()
+        {
+
+        }
+
+        [TestMethod]
+        public void CanDeletePublishedAd()
+        {
+
+        }
+
+        [ClassCleanup()]
+        public static void TearDown()
+        {
+            Browser.Close();
+        }
+    }
+}
